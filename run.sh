98fabc0df88b09ebc7e4f28ca2e147f2df749855
Yujie Zhou <143871992+EpsilonZYJ@users.noreply.github.com>
2025-11-18 15:29:34 +0800

    init lab3_2

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ echo "=============================Start Compile================================
 make clean
 make
 echo "============================Compile Complete==============================="
-spike ./obj/riscv-pke ./obj/app_naive_fork
+spike ./obj/riscv-pke ./obj/app_yield
