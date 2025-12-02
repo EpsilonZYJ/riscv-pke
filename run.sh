96c62d3542adda91131e5fb620ab93bbacd709fc
Yujie Zhou <143871992+EpsilonZYJ@users.noreply.github.com>
2025-12-02 16:02:01 +0800

    init lab4_challenge1

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ echo "=============================Start Compile================================
 make clean
 make
 echo "============================Compile Complete==============================="
-spike ./obj/riscv-pke ./obj/app_hardlink
+spike ./obj/riscv-pke ./obj/app_relativepath
