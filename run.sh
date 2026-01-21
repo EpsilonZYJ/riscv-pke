22cbc32d0d3e5effa96143c1f47ddf0fae82be14
Yujie Zhou <143871992+EpsilonZYJ@users.noreply.github.com>
2026-01-21 14:34:55 +0800

    add run script

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ echo "=============================Start Compile================================
 make clean
 make
 echo "============================Compile Complete==============================="
-spike ./obj/riscv-pke ./obj/app_long_loop
+spike ./obj/riscv-pke ./obj/app_erroline
