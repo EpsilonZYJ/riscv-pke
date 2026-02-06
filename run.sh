9d139395d647d54ccc9d813e4dbad03099d758a9
Yujie Zhou <143871992+EpsilonZYJ@users.noreply.github.com>
2026-02-06 23:50:13 +0800

    feat: init lab

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 echo "=============================Start Compile================================="
 make clean
 make
-rm -rf output.txt
+#rm -rf output.txt
 echo "============================Compile Complete==============================="
 spike obj/riscv-pke /bin/app_shell
